a366c497df8d98e409dc519940798d7f9e025ebd
Hulme <kacy.hulme@gmail.com>
2018-01-10 21:57:26 -0600

    add mock routes for navigation

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ var app = express();
 
 app.use(express.static("public"));
 
+app.get("/service", function(req, res) {
+  res.send("Service");
+});
+app.get("/portfolio", function(req, res) {
+  res.send("Portfolio");
+});
+app.get("/team", function(req, res) {
+  res.send("Team");
+});
+app.get("/blog", function(req, res) {
+  res.send("Blog");
+});
+app.get("/contact", function(req, res) {
+  res.send("Service");
+});
+
 app.listen(3000, function() {
   console.log("Express server is up on port 3000");
 });
